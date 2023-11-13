2e1c15c19e115e41c040c72320602802e4928b69
Ibraheem Tarek <ibraheemtarek99@yahoo.com>
2023-11-13 13:10:30 -0800

    fixing varialbes

--- print_int.c
+++ print_int.c
@@ -6,14 +6,16 @@
  */
 int print_int(va_list v1)
 {
-	int n = va_arg(v1, int);
-	int num, last = n % 10, digit, hv = 1;
+	int x = va_arg(v1, int);
+	int n, last_digit = x % 10,
+	int dig;
+       	int hv = 1;
 	int  i = 1;
 
 	n = n / 10;
 	num = n;
 
-	if (last < 0)
+	if (last_digit < 0)
 	{
 		_putchar('-');
 		num = -num;
