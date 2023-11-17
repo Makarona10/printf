f18abcfef28b4c6a74d974fbf49c4b9d4564c062
Ibraheem Tarek <ibraheemtarek99@yahoo.com>
2023-11-17 05:57:12 -0800

    s

---
+++ p_bin.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * p_bin - prints binary number.
+ *
+ * @vl: parameter.
+ *
+ * Return: integer
+ */
+int p_bin(va_list vl)
+{
+	int flags = 0;
+	int count = 0;
+	int k, a = 1, b;
+	unsigned int n = va_arg(vl, unsigned int);
+	unsigned int x;
+
+	for (k = 0; k < 32; k++)
+	{
+		x = ((a << (31 - i)) & n);
+		if (x >> (31 - i))
+			flags = 1;
+		if (flags)
+		{
+			b = x >> (31 - k);
+			_putchar(b + 48);
+			count++;
+		}
+	}
+	if (count == 0)
+	{
+		count++;
+		_putchar('0');
+	}
+	return (count);
+}
